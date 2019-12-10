43687275052e46072b7d4ef0b39999f62695b7e2
Deepak Kafle <dkafle@gmail.com>
2019-12-09 23:08:24 -0500

    Day-02 Work

--- src/index.js
+++ src/index.js
@@ -10,15 +10,21 @@ const Greeter = (props) => {
   return <div className="firstClass">
     <h1> Hello, I {props.emotion} {props.libraryName} </h1>
     <p>I really really {props.emotion} {props.libraryName}</p>
-    <Login />
+    <button onClick={props.doSomething}>hello</button>
   </div >;
 };
 
 class ShoppingList extends React.Component {
+  doSomething(event) {
+    console.log('doing something');
+  }
+
   render() {
+    let n = this.props.name;
+    const lib = 444444;
     return (<div>
-      <Greeter emotion="Love" libraryName="ReactJS" />
-      <h1> Shopping List for {this.props.name} </h1>
+      <Greeter libraryName={lib} emotion="Love" doSomething={this.doSomething} />
+      <h1> Shopping List for {n} </h1>
       <ul>
         <li > Instagram </li>
         <li> WhatsApp </li>
