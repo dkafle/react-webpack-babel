2b20b5fc1406ad107bd6e2c0dd2e98e1452b873b
Deepak Kafle <dkafle@gmail.com>
2019-12-12 22:39:42 -0500

    Square components maintain theri own state

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clickCounts: 0,
+    };
+  }
+
+  showMessage() {
+    console.log(`You have counted ${this.props.value} button ${this.state.clickCounts} times`)
+  }
+
   render() {
     return (
-      <button className="square">
+      <button
+        className="square"
+        onClick={() => {
+          let count = this.state.clickCounts;
+          count += 1;
+          this.setState({
+            clickCounts: count,
+          }, this.showMessage)    //Passing showMessage as callback
+          //this.showMessage();
+        }}
+      >
         {this.props.value}
       </button>
     );
