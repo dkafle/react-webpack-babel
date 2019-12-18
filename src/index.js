2a66d896f8f6669d33c2c88b3b45f0bb9e260202
Deepak Kafle <dkafle@gmail.com>
2019-12-18 08:06:06 -0500

    function component vs class component

--- src/index.js
+++ src/index.js
@@ -1,34 +1,76 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Login = () => (<div>
-  <p>Login</p>
-  <p>Signup</p>
-</div>);
-
-const Greeter = (props) => {
-  return <div className="firstClass">
-    <h1> Hello, I {props.emotion} {props.libraryName} </h1>
-    <p>I really really {props.emotion} {props.libraryName}</p>
-    <Login />
-  </div >;
+// Function Components
+/*
+function Welcome(props) {
+	return <h1>You are welcome, {props.name}</h1>;
+}
+const Welcome = (props) => {
+	const userName = props.name;
+	return(<>
+		<h1>You are welcome, {userName}</h1>
+		<p>hello this is my p gat</p>
+	</>);
 };
+*/
 
-class ShoppingList extends React.Component {
-  render() {
-    return (<div>
-      <Greeter emotion="Love" libraryName="ReactJS" />
-      <h1> Shopping List for {this.props.name} </h1>
-      <ul>
-        <li > Instagram </li>
-        <li> WhatsApp </li>
-        <li> Oculus </li>
-      </ul>
-    </div>);
+function ActionLink() {
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('The link was clicked.');
   }
+
+  return (
+    <a href="#" onClick={handleClick}>
+      Click me
+    </a>
+  );
+}
+
+// Class Component
+class Welcome extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+      isClicked: false,
+      hasMounted: false
+		};
+  }
+
+  componentDidMount() {
+    this.setState({hasMounted: true});
+  }
+
+  componentWillUnmount() {
+    console.log('im getting unloaded');
+  }
+
+  showConsole() {
+    console.log(this.state);
+    // call all your methds from here
+  }
+
+	updateCount() {
+		const newState = {
+			isClicked: true,
+		}
+    this.setState(newState, this.showConsole);
+    // this.setState(newState);
+    // console.log(this.state);
+	}
+
+	render() {
+		const userName = this.props.name;
+    return (<div>
+      <h1 onClick={() => this.updateCount()}>
+        Welcome, {userName}</h1>
+      <ActionLink>Hello</ActionLink>
+    </div>);
+	}
 }
 
 ReactDOM.render(
-  <ShoppingList name="Deepak" />,
+  <Welcome name="Rajiv" />,
   document.getElementById('app')
 );
