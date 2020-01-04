10d2a1b251b1e863d61bce18fe49ea1a393f6333
Deepak Kafle <dkafle@gmail.com>
2020-01-03 23:36:03 -0500

    Maintain state locally

--- src/app.jsx
+++ src/app.jsx
@@ -6,12 +6,36 @@ import "styles/base/_common.sass"  // Global styles
 import styles from "./app.sass"  // Css-module styles
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: 21
+    };
+  }
+
+  onAgeUp() {
+    let age = this.state.age;
+    this.setState({
+      age: ++age,
+    });
+  }
+
+  onAgeDown() {
+    let age = this.state.age;
+    this.setState({
+      age: --age,
+    });
+  }
 
   render() {
     return (<div className='App'>
       <div>
         <h1>Understand React Redux</h1>
-        <div></div>
+        <div>
+          <div>Your age is: {this.state.age}</div>
+          <button onClick={() => this.onAgeUp()}>Age Up</button>
+          <button onClick={() => this.onAgeDown()}>Age Down</button>
+        </div>
       </div>
     </div>);
   }
