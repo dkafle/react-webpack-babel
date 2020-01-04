673e10490cc2cc26f72acefbec16cd14c6314ef0
Deepak Kafle <dkafle@gmail.com>
2020-01-04 11:25:54 -0500

    Updated code for debugging

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducer from './redux/reducer';
 
-const myStore = createStore(reducer);
+const myStore = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 
 ReactDOM.render(
