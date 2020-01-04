b9434d2aee02102d332fad08cc0dd2d0a37823e2
Deepak Kafle <dkafle@gmail.com>
2020-01-04 04:01:16 -0500

    Connect App with Store

--- src/app.jsx
+++ src/app.jsx
@@ -1,44 +1,58 @@
 import React from 'react';
 import 'normalize.css';
+import {connect} from 'react-redux';
 
 import "styles/base/_main.sass"  // Global styles
 import "styles/base/_common.sass"  // Global styles
 // import styles from "./app.sass"  // Css-module styles
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      age: 21
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     age: 21
+  //   };
+  // }
 
-  onAgeUp() {
-    let age = this.state.age;
-    this.setState({
-      age: ++age,
-    });
-  }
+  // onAgeUp() {
+  //   let age = this.state.age;
+  //   this.setState({
+  //     age: ++age,
+  //   });
+  // }
 
-  onAgeDown() {
-    let age = this.state.age;
-    this.setState({
-      age: --age,
-    });
-  }
+  // onAgeDown() {
+  //   let age = this.state.age;
+  //   this.setState({
+  //     age: --age,
+  //   });
+  // }
 
   render() {
     return (<div className='App'>
       <div>
         <h1>Understand React Redux</h1>
         <div>
-          <div>Your age is: {this.state.age}</div>
-          <button onClick={() => this.onAgeUp()}>Age Up</button>
-          <button onClick={() => this.onAgeDown()}>Age Down</button>
+          <div>Your age is: {this.props.age}</div>
+          <button onClick={this.props.onAgeUp}>Age Up</button>
+          <button onClick={this.props.onAgeDown}>Age Down</button>
         </div>
       </div>
     </div>);
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    age: state.age,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAgeUp: () => dispatch({ type: 'AGE_UP' }),
+    onAgeDown: () => dispatch({ type: 'AGE_DOWN' }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
