078a97a9dacd3e1505f0d31ddc57b267d14baf54
dkafle <dkafle@gmail.com>
2020-01-11 02:19:28 +0545

    get remote data

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import 'normalize.css';
 import {connect} from 'react-redux';
+import axios from 'axios';
 
 import "styles/base/_main.sass"  // Global styles
 import "styles/base/_common.sass"  // Global styles
 // import styles from "./app.sass"  // Css-module styles
 import MovieManager from './components/MovieList';
+
+const API_KEY = 'TsBcDznUA5bcQK7u7lYRwEcIvCqHHXmM';
+const random_gif = 'http://api.giphy.com/v1/gifs/random';
 
 class App extends React.Component {
   constructor(props) {
@@ -16,11 +20,29 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    
+    this.getRemoteData();
   }
+
+  getRemoteData() {
+    const url = random_gif + '?api_key=' + API_KEY;
+    axios.get(url)
+      .then(result => {
+        console.log(result);
+        this.setState({
+          giffy: result.data,
+        })
+      });
+  }
+
   render() {
+    const giffy = this.state.giffy;
     return (<div className='App'>
       <div>
+        <div>
+          <h3>{giffy.data ? giffy.data.title : 'No title'}</h3>
+          <img src={giffy.data ? giffy.data.image_url : ''} /> <br/>
+          <button onClick={() =>this.getRemoteData()}>Get Next</button>
+        </div>
         <h1>Understand React Redux</h1>
         <div>
           <div>Your age is: {this.props.age}</div>
